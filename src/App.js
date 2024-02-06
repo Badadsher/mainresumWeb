6a955d372901d3be71bd8573b30727f3c9cdfb6c
Joe Biden <dominserv123@mail.ru>
2024-02-06 16:56:54 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
+
 function App() {
   return (
     <React.Fragment>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,28 +1,14 @@
 import "../Components/Styles/hero.css";
-import backgroundImage from "../img/gradi.png";
-import example1 from "../img/examples/dog.png";
-import example2 from "../img/examples/newbattle.png";
-import example3 from "../img/examples/newhome.png";
-import example4 from "../img/examples/car.jpg";
-import example5 from "../img/examples/PLANENEW.png";
-import example6 from "../img/examples/samurai.png";
-import example7 from "../img/examples/night.png";
-import example8 from "../img/examples/samuraiweb.png";
-import example9 from "../img/examples/verh.jpg";
-
+import webchai from "../img/examples/chai.png";
 import web1 from "../img/examples/site.png";
 import web2 from "../img/examples/kino.png";
 import web3 from "../img/examples/5.png";
 import web4 from "../img/examples/4.png";
-
+import webrespect from "../img/examples/pcresp.png";
 import vkicon from "../img/icons/vk.png";
 import tgicon from "../img/icons/tg.png";
 import giticon from "../img/icons/git.png";
 function Main() {
-  const MyStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-  };
-
   return (
     <main>
       <div className="all">
@@ -31,17 +17,23 @@ function Main() {
             <h1>ПОРТФОЛИО</h1>
           </div>
 
-          {/* <nav className="examples">
-              <img className="example"></img>
-              <img className="example" src={example1} alt="dog"></img>
-              <img className="example"></img>
-            </nav> */}
-
           <div className="naming">
             <h1>ВЕБ ПРИМЕРЫ</h1>
           </div>
 
           <div className="examples " id="menu">
+            <div>
+              <a href="https://github.com/Badadsher/chaihona">
+                <img className="example" src={webchai}></img>
+              </a>
+            </div>
+
+            <div>
+              <a href="https://github.com/Badadsher/pcrespect">
+                <img className="example" src={webrespect}></img>
+              </a>
+            </div>
+
             <div>
               <a href="https://github.com/Badadsher/MovieExample/tree/main">
                 <img className="example" src={web2}></img>
@@ -63,37 +55,6 @@ function Main() {
             </div>
           </div>
 
-          <div className="naming">
-            <h1>ПРИМЕРЫ ИГР</h1>
-          </div>
-
-          <div className="examples " id="menu">
-            <div>
-              <img className="example" src={example1} alt="dog"></img>
-            </div>
-            <div>
-              <img className="example" src={example2} alt="battle"></img>
-            </div>
-            <div>
-              <img className="example" src={example3} alt="home"></img>
-            </div>
-            <div>
-              <img className="example" src={example4} alt="car"></img>
-            </div>
-            <div>
-              <img className="example" src={example5} alt="plane"></img>
-            </div>
-            <div>
-              <img className="example" src={example6} alt="samurai"></img>
-            </div>
-            <div>
-              <img className="example" src={example7} alt="dog"></img>
-            </div>
-            <div>
-              <img className="example" src={example8} alt="battle"></img>
-            </div>
-          </div>
-
           <nav className="about-menu " id="about">
             <div className="abouter">
               <h1>ОБО МНЕ</h1>
@@ -102,12 +63,11 @@ function Main() {
               {" "}
               <p>
                 Приветствую! Этот сайт является портфолио с примерами моих
-                работ. Я являюсь фронтенд разработчиком и разработчиком игр на
-                движке Unity. Вы можете просмотреть примеры моих работ на
-                гитхабе. На данный момент я зиждлюсь на
-                Unity,C#,HTML,JS,ReactJS,TypeScript. Также я имею опыт работы с
-                заказчиками на фриланс биржах и командной работы. На данный
-                момент мой опыт программирования превышает цифру в один год.
+                работ. Я являюсь фронтенд разработчиком. Вы можете просмотреть
+                примеры моих работ на гитхабе. На данный момент мой стек состоит
+                из: HTML, JS, REDUX, React, TypeScript, CSS/SASS. Также я имею
+                опыт на фриланс биржах и командной работы составляющий
+                длительность в более чем один год.
               </p>
             </div>
           </nav>
@@ -115,20 +75,20 @@ function Main() {
           <nav className="contacts-menu" id="contact">
             <h1>КОНТАКТЫ</h1>
             <nav className="social-medias">
-              <a href="https://t.me/infinityhybrid">
+              <a href="https://t.me/necrasfril">
                 <img src={tgicon} className="socialic"></img>
               </a>
 
               <a href="https://github.com/Badadsher">
                 <img src={giticon} className="socialic"></img>
               </a>
-              <a href="https://vk.com/infinityhybrid">
+              <a href="https://vk.com/nasyrovbdash">
                 <img src={vkicon} className="socialic"></img>
               </a>
             </nav>
           </nav>
         </div>
-        <div className="bg-img" style={MyStyle}></div>
+        <div id="bg-img" className="bg-img"></div>
       </div>
     </main>
   );
